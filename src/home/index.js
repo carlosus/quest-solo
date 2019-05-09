ac798ba889feb087273d8c8acce840b77cd0c3d8
Carlos Us Santiago <cdus12@gmail.com>
2019-05-09 11:29:16 -0700

    separated form-data test into its own file

--- src/home/index.js
+++ src/home/index.js
@@ -1,18 +1,19 @@
-import makeQueen from './make-queen.js';
 import api from '../services/api.js';
+import makeQueen from './make-queen.js';
 
 // Handle Form Submit and Log User Object
 
 // Reference the form from the DOM
 const queenForm = document.getElementById('user-sign-up');
 queenForm.addEventListener('submit', event => {
-// Subscribe to submit event (don't forget to prevent default action!)
+    // Subscribe to submit event (don't forget to prevent default action!)
     event.preventDefault();
-    
+
     // Make formData from form
     const formData = new FormData(queenForm);
     // Call makeUser
     const queen = makeQueen(formData);
+
     // Form event calls api.sigUp to save user
     api.signUp(queen);
 });
--- test/tests.js
+++ test/tests.js
@@ -1,26 +1,2 @@
-import makeQueen from '../src/home/make-queen.js';
 import './api.test.js';
-
-const test = QUnit.test;
-
-test('create user from form data', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const formData = new FormData();
-    formData.set('name', 'Carlos');
-    formData.set('character', 'Valentina');
-
-    const expected = {
-        name: 'Carlos',
-        character: 'Valentina',
-        lifeLine: '50',
-        cashTip: '0'
-    };
-
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const queen = makeQueen(formData);
-
-    //Assert
-    assert.deepEqual(queen, expected);
-});
+import './form-data.test.js';
