379e882fc9d4a5913848b472b80bca26a94ffe92
Carlos Us Santiago <cdus12@gmail.com>
2019-05-12 19:26:46 -0700

    load quest list. Added functionality to load available quests into the map page

--- src/map/map.js
+++ src/map/map.js
@@ -1,3 +1,15 @@
+import api from '../services/api.js';
 import loadProfile from '../load-profile.js';
+import createQuestLink from '../map/create-quest-link.js';
 
 loadProfile();
+
+const quests = api.getQuests();
+
+const nav = document.getElementById('quests');
+
+for(let i = 0; i < quests.length; i++) {
+    const quest = quests[i];
+    const link = createQuestLink(quest);
+    nav.appendChild(link);
+}
